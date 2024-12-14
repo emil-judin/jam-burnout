2da8f822fd2ad39dd6c253537946bdd116bd9d8f
Kumonari <184982588+Kumonari@users.noreply.github.com>
2024-12-14 20:32:24 +0100

    added endscreen + highscorelist

--- burnout/scripts/ui.gd
+++ burnout/scripts/ui.gd
@@ -1,12 +1,11 @@
 class_name UI
 extends CanvasLayer
 
-
-
-@export var wait_time: float = 290.0  # seconds
+@export var wait_time: float = 10.0  # seconds
 @export var max_time: float = 300.0
 
 var timer = null
+static var game_start_time = Time.get_ticks_msec()
 
 @onready var countdown_label: RichTextLabel = $Control/MainContainer/TopBox/MarginContainer2/Countdown
 @onready var progress_bar: ProgressBar = $Control/ProgressBarContainer/MarginContainer/ProgressBar
@@ -75,7 +74,28 @@ func subtract_time(seconds: float) -> void:
 		transition_to_end()
 
 
+static func run_time_output() -> String:
+	var run_time = Time.get_ticks_msec() - game_start_time
+	
+	var min = run_time/1000/60
+	var min_output = str(min)
+	var sec = run_time/1000%60
+	var sec_output = str(sec)
+	var msec = run_time%1000/10
+	var msec_output = str(msec)
+
+	# Sorgt dafür das die Zahlen richtig dargestellt werden wenn sie unter 9 sind.
+	if min <= 9:
+		min_output = "0" + str(min)
+		
+	if sec <= 9:
+		sec_output = "0" + str(sec)
+		
+	if msec <= 9:
+		msec_output = "0" + str(msec)
+	
+	# Gibt die einzelnen Strings als zusammengesetzten String zurück.
+	return min_output+ ":" + sec_output+ ":" + msec_output
+
 func transition_to_end() -> void:
-	pass
-	# save run time to file
-	# change scene to endscreen
+	get_tree().call_deferred("change_scene_to_file", "res://scenes/game/EndScreen.tscn")
