1b5c8f335a32c5ed3647e9eae7a0757d71c4aad7
Emil Judin <83183112+emil-judin@users.noreply.github.com>
2024-12-14 22:25:38 +0100

    Connect timer to player

--- burnout/scripts/player.gd
+++ burnout/scripts/player.gd
@@ -2,8 +2,40 @@ extends CharacterBody2D
 class_name Player
 
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+@export var ui: UI
 @export var movement_speed: float = 400
-#@export var contact_damage_cooldown: float = 2
+@export var max_health_time: float = 300.0
+@export var start_health_time: float = 10.0  # seconds
+var health_timer: Timer = null
+
+func _ready():
+	health_timer = Timer.new()
+	health_timer.set_wait_time(start_health_time)
+	add_child(health_timer)
+	health_timer.connect("timeout", func(): _on_timer_timeout())
+	ui.initialize_health_ui(start_health_time, max_health_time)
+	health_timer.start()
+
+func _process(delta):
+	if !health_timer.is_stopped():
+		ui.update_countdown(health_timer.time_left)
+		ui.update_progress_bar(health_timer.time_left)
+
+func add_heatlth_time(seconds: float) -> void:
+	if health_timer.time_left + seconds <= max_health_time:
+		health_timer.start(health_timer.time_left + seconds)
+	else:
+		health_timer.start(max_health_time)
+
+func subtract_health_time(seconds: float) -> void:
+	if health_timer.time_left - seconds > 0:
+		health_timer.start(health_timer.time_left - seconds)
+	else:
+		_on_timer_timeout()
+
+func _on_timer_timeout():
+	health_timer.stop()
+	get_tree().call_deferred("change_scene_to_file", "res://scenes/game/EndScreen.tscn")
 
 func _physics_process(delta):
 	var direction = Input.get_vector("left", "right", "up", "down")
@@ -22,15 +54,27 @@ func _physics_process(delta):
 	velocity = direction * movement_speed
 	move_and_slide()
 	
+	if Input.is_action_just_pressed("+"):
+		add_heatlth_time(10)
+	if Input.is_action_just_pressed("-"):
+		subtract_health_time(10)
+	
 func time_item_collected(time_value):
-	print("added " + time_value + " collected")
+	add_heatlth_time(time_value)
 
 func _on_area_entered(area: Area2D):
 	var parent = area.get_parent()
 	if parent is TimeItem:
 		var time_item = parent as TimeItem
 		print("inceasing timer by " + str(time_item.time_value))
+		time_item_collected(time_item.time_value)
 		time_item.collect()
 	if parent is Enemy || parent is JumpingEnemy:
 		# Deal contact damage
 		print("decreasing timer by " + str(parent.contact_damage))
+		subtract_health_time(parent.contact_damage)
+	if parent is EnemyBullet:
+		# Deal bullet damage
+		var enemy_bullet = parent as EnemyBullet
+		print("decreasing timer by " + str(enemy_bullet.damage))
+		subtract_health_time(enemy_bullet.damage)
--- burnout/scripts/ui.gd
+++ burnout/scripts/ui.gd
@@ -1,78 +1,40 @@
 class_name UI
 extends CanvasLayer
 
-@export var wait_time: float = 10.0  # seconds
-@export var max_time: float = 300.0
-
-var timer = null
 static var game_start_time = Time.get_ticks_msec()
 
-@onready var countdown_label: RichTextLabel = $Control/MainContainer/TopBox/MarginContainer2/Countdown
-@onready var progress_bar: ProgressBar = $Control/ProgressBarContainer/MarginContainer/ProgressBar
+@export var countdown_label: RichTextLabel
+@export var progress_bar: ProgressBar
 
-func _ready():
-	progress_bar.max_value = max_time
-	timer = Timer.new()
-	timer.set_wait_time(wait_time)
-	add_child(timer)
-	timer.connect("timeout", func(): _on_timer_timeout()) 
-	timer.start()
+func initialize_health_ui(start_health_time: float, max_health_time: float):
+	progress_bar.max_value = max_health_time
+	update_countdown(start_health_time)
+	update_progress_bar(start_health_time)
 
-func _process(delta):
-	update_countdown()
-	update_progress_bar()
+func update_countdown(time_left: float) -> void:
+	var current_time = time_left
+	var min = int(current_time) / 60
+	var min_output = str(min)
+	var sec = int(current_time) - min * 60
+	var sec_output = str(sec)
+	var msec = int((snappedf(current_time, 0.01) - sec - min * 60) *100)
+	var msec_output = str(msec)
 	
-	if Input.is_action_just_pressed("+"):
-		add_time(10)
-	if Input.is_action_just_pressed("-"):
-		subtract_time(10)
+	if min <= 9:
+		min_output = "0" + min_output
+	
+	if sec <= 9:
+		sec_output = "0" + sec_output
+	
+	if msec == 100:
+		msec_output = "00"
+	if msec <= 9:
+		msec_output = "0" + msec_output
+	
+	countdown_label.text = min_output + ":" +  sec_output + ":" + msec_output
 
-func update_countdown() -> void:
-	if timer:
-		var current_time = timer.time_left
-		var min = int(current_time) / 60
-		var min_output = str(min)
-		var sec = int(current_time) - min * 60
-		var sec_output = str(sec)
-		var msec = int((snappedf(current_time, 0.01) - sec - min * 60) *100)
-		var msec_output = str(msec)
-		
-		if min <= 9:
-			min_output = "0" + min_output
-		
-		if sec <= 9:
-			sec_output = "0" + sec_output
-		
-		if msec == 100:
-			msec_output = "00"
-		if msec <= 9:
-			msec_output = "0" + msec_output
-		
-		countdown_label.text = min_output + ":" +  sec_output + ":" + msec_output
-
-func update_progress_bar() -> void:
-	if timer:
-		progress_bar.value = timer.time_left 
-
-func _on_timer_timeout() -> void:
-	timer.stop()
-	transition_to_end()
-
-
-func add_time(seconds: float) -> void:
-	if timer.time_left + seconds <= max_time:
-		timer.start(timer.time_left + seconds)
-	else:
-		timer.start(max_time)
-
-
-func subtract_time(seconds: float) -> void:
-	if timer.time_left - seconds > 0:
-		timer.start(timer.time_left - seconds)
-	else:
-		timer.stop()
-		transition_to_end()
-
+func update_progress_bar(time_left: float) -> void:
+	progress_bar.value = time_left
 
 static func run_time_output() -> String:
 	var run_time = Time.get_ticks_msec() - game_start_time
@@ -96,6 +58,3 @@ static func run_time_output() -> String:
 	
 	# Gibt die einzelnen Strings als zusammengesetzten String zurück.
 	return min_output+ ":" + sec_output+ ":" + msec_output
-
-func transition_to_end() -> void:
-	get_tree().call_deferred("change_scene_to_file", "res://scenes/game/EndScreen.tscn")
