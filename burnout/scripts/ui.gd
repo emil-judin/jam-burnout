53231df713321809bb16ce6bee85d185421facc4
Kumonari <184982588+Kumonari@users.noreply.github.com>
2024-12-14 10:04:27 +0100

    add an substract time functions implemented

--- burnout/scripts/ui.gd
+++ burnout/scripts/ui.gd
@@ -2,14 +2,15 @@ extends CanvasLayer
 
 
 
-@export var start_time: float = 120.0  # seconds
+@export var wait_time: float = 5.0  # seconds
 var timer = null
 @onready var countdown_label: RichTextLabel = $Control/VBoxContainer/Top/MarginContainer2/Countdown
 
 func _ready():
 	timer = Timer.new()
-	timer.set_wait_time(start_time)
+	timer.set_wait_time(wait_time)
 	add_child(timer)
+	timer.connect("timeout", func(): _on_timer_timeout()) 
 	timer.start()
 
 func _process(delta):
@@ -32,7 +33,25 @@ func _process(delta):
 			msec_output = "0" + msec_output
 		
 		countdown_label.text = min_output + ":" +  sec_output + ":" + msec_output
+		
+		if Input.is_action_just_pressed("+"):
+			add_time(10)
+		if Input.is_action_just_pressed("-"):
+			subtract_time(10)
 
 func _on_timer_timeout():
 	print("Countdown complete!")
 	timer.stop()
+
+
+func add_time(seconds: float) -> void:
+	timer.start(timer.time_left + seconds)
+
+
+func subtract_time(seconds: float) -> void:
+	if timer.time_left - seconds > 0:
+		"timer"
+		timer.start(timer.time_left - seconds)
+	else:
+		print("timer 0")
+		timer.stop()
