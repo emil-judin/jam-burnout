2afdb99a1810e424a8cb0c2c9de3b9b2084015cc
Kumonari <184982588+Kumonari@users.noreply.github.com>
2025-02-05 12:06:04 +0100

    BUGFIX Doppelclick auf buttons crashed das Spiel

--- burnout/scripts/main_menu.gd
+++ burnout/scripts/main_menu.gd
@@ -9,7 +9,7 @@ extends Node
 func _on_start_button_clicked() -> void:
 	ClickSound.play()
 	await ClickSound.finished
-	get_tree().change_scene_to_file(intro_scene_path)
+	get_tree().call_deferred("change_scene_to_file", intro_scene_path)
 
 
 func _on_quit_button_clicked() -> void:
